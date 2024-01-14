3156fde3b021a28eec39feaf7b136394b9625e5e
AinurAsyirah <2023388507@student.uitm.edu.my>
2024-01-14 12:14:05 +0800

    add loginController

---
+++ curricular-system/src/main/java/student/controller/LoginController.java
@@ -0,0 +1,64 @@
+package student.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import student.dao.StudentDAO;
+import student.model.StudentBean;
+
+/**
+ * Servlet implementation class LoginController
+ */
+@WebServlet("/LoginController")
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private StudentDAO dao; 
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginController() {
+        super();
+        dao= new StudentDAO();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		String username = request.getParameter("login-username");
+		String password = request.getParameter("login-password");
+		
+		StudentBean student = StudentDAO.findByLogin(username,password);
+		StudentBean stud = StudentDAO.getStudentById(username);
+		
+		
+		if(student != null) {
+			//valid login, redirect dashboard student
+			HttpSession session = request.getSession();
+			session.setAttribute("Login", student);
+			request.setAttribute("dashboard", stud);
+			response.sendRedirect("dashboardStudent.jsp?studentIC=" + username +"&studentName=" + stud.getStudentName());
+		}else {
+			//invalid login, show error message
+			request.setAttribute("error", "Invalid login");
+			request.getRequestDispatcher("invalidPassword.jsp").forward(request,response);
+		}
+	}
+
+}
